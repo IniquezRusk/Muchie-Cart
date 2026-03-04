12fa591bc17f95778c4ab01d2f755c0b99b62e05
Iniquez Rusk Jr <iniquezrusk@gmail.com>
2026-03-03 23:07:43 -0600

    Refactor loadMenu and addToCart functions

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
 async function loadMenu() {
+
     const response = await fetch("http://localhost:3000/menu");
     const menu = await response.json();
 
     const menuDiv = document.getElementById("menu");
 
     menu.forEach(item => {
+
         const card = document.createElement("div");
 
         card.innerHTML = `
         <h3>${item.name}</h3>
         <p>$${item.price}</p>
         <p>Ingredients: ${item.ingredients.join(", ")}</p>
-        <button onclick="addToCart(${item.id})>Add To Cart</button>
+        <button onclick="addToCart(${item.id})">Add To Cart</button>
         `;
 
         menuDiv.appendChild(card);
+
     });
- }
+}
 
- let cart = JSON.parse(localStorage.getItem("cart")) || [];
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
- function addToCart(id) {
+function addToCart(id) {
+
     cart.push(id);
     localStorage.setItem("cart", JSON.stringify(cart));
 
     alert("Added to cart!");
- }
+}
 
+loadMenu();
